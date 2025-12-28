4df29379c6fc2a570dc9d45b6651e0bcab8d76fe
TomasLavadosSepulveda <tilavados@uc.cl>
2025-12-28 17:21:23 -0300

    Create titles-map.js

---
+++ didactic-eureka/js/titles-map.js
@@ -0,0 +1,5 @@
+window.TITLES_MAP = {
+  "intuicionintelectuallibertad": "Intuición Intelectual y Libertad",
+  "lapersonahumanaensantotomasdeaquino": "La Persona Humana en Santo Tomás de Aquino",
+  "metafisicaformalc": "Metafísica Formal Cuántica"
+};
